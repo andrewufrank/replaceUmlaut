17326e4fac479f33f377075aa6a5ac07ea643c53
andrew u frank <frank@london.local>
2020-05-21 20:33:20 +0200

    mergin the monads errio and pandoc

--- Lib/ProcPandocDatei.hs
+++ Lib/ProcPandocDatei.hs
@@ -22,29 +22,37 @@ import           Lib.ProcTxt                    ( txtFile )
 -- for pandoc testing
 import qualified Text.Pandoc                   as P
 import qualified Text.Pandoc.Walk              as PW
-import qualified Data.Text                     as T
-import qualified Data.Text.IO                  as TIO
+-- import qualified Data.Text                     as T
+-- import qualified Data.Text.IO                  as TIO
 
 data Markdown = Markdown Text deriving (Show, Read, Eq, Ord)
 -- ^ just for marking md files
 mdFile :: TypedFile5 Text Markdown
+mdExtension :: Extension
 mdExtension = Extension "md" :: Extension
+bakExtension :: Extension
 bakExtension = Extension "bak" :: Extension
+newExtension :: Extension
 newExtension = Extension "new" :: Extension
 mdFile = makeTyped mdExtension
 -- ^ filetype to read text in lines
 
-readPandoc :: [Text] -> Markdown -> IO Markdown
-readPandoc erlaubt dat = do
+chnageUmlautInPandoc :: Bool -> [Text] -> Markdown -> IO Markdown
+-- ^ change in all text in a md file the umlaute
+-- erlaubt is a list of words where there is no umlaut (but ae, oe or ue)
+chnageUmlautInPandoc debug erlaubt dat = do
   result <- P.runIO $ do
-    liftIO $ TIO.putStrLn . showT $ dat
+    when debug $ putIOwords ["changeUmlautInPandoc", take' 100 . showT $ dat]
+        -- liftIO $ TIO.putStrLn . showT $ dat
     doc <- P.readMarkdown P.def (unwrap7 dat) -- (T.pack "[testing](url)")
-    liftIO $ TIO.putStrLn . showT $ doc
+    when debug $ putIOwords ["changeUmlautInPandoc", take' 100 . showT $ doc]
+      -- liftIO $ TIO.putStrLn . showT $ doc
     -- here the processing
     let doc2 = umlautenStr erlaubt doc
     P.writeMarkdown P.def doc2
   rst <- P.handleError result
-  TIO.putStrLn rst
+  when debug $ putIOwords ["changeUmlautInPandoc end", take' 100 . showT $ rst]
+  -- TIO.putStrLn rst
   return . wrap7 $ rst
 
 umlautenStr :: [Text] -> P.Pandoc -> P.Pandoc
@@ -60,11 +68,11 @@ procMd :: Bool -> Path Abs File -> Path Abs File -> ErrIO ()
 -- in a file with extension txt
 procMd debug fnErl fn = do
   erl <- read6 fnErl txtFile -- reads lines
-  let erlaubt = concat . map words' $ erl :: [Text]
+  let erl2 = concat . map words' $ erl :: [Text]
 
   ls :: Markdown <- read8 fn mdFile
   putIOwords ["procMd ls", showT ls, "fn", showT fn]
-  res <- liftIO $ readPandoc erl ls
+  res <- liftIO $ chnageUmlautInPandoc debug erl2 ls
 
   if debug
     then do
@@ -80,7 +88,7 @@ procMd debug fnErl fn = do
   -- let ls2      = map (procLine erlaubt) ls
 
   -- write6 fn txtFile ls2
-  putIOwords ["procMd done", showT res]
+  when debug $ putIOwords ["procMd done", showT res]
 
 procLine :: [Text] -> Text -> Text
 procLine erlaubt ln = unwords' . map (procWord2 erlaubt) . words' $ ln
