c086bf4e58b623ac3ce108f1a080d5b074746601
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2023-02-19 16:24:08 +0100

    generalized for txt and md

--- src/Lib/ProcTxt.hs
+++ src/Lib/ProcTxt.hs
@@ -23,49 +23,29 @@ import Uniform.Pandoc (extMD)
 
 procTxt :: Bool -> [Text] -> Path Abs File -> ErrIO ()
 -- ^ replace umlaut unless it is an permitted group
--- in a file with extension txt (only!)
+-- in a file with extension txt or md (only!)
 procTxt debug erl2 fn = do
     when debug $ putIOwords ["procTxt start", showT fn]
     let fnExtension = getExtension fn :: Extension
     
-    -- erl2         <- readErlaubt fnErl
-    -- erl  <- read6 fnErl txtFile -- reads lines
-    -- let erlaubt = concat . map words' $ erl :: [Text]
-    -- (fnext, fnbak, fnnew) <- changeExtensionBakOrNew debug fn
+    let textLineType = if fnExtension == txtExtension 
+        then textlinesFile 
+        else if fnExtension == extMD 
+            then mdFile 
+            else errorT  ["ERROR: not txt file - nothing done!"]
 
-    if fnExtension == txtExtension 
-        then do 
-    --     then do
-            ls :: [Text] <- read8 fn textlinesFile  -- split in lines
-        --         return ls
-        -- else do 
-        --         ls  <- read8 fn mdFile
-        --         return ls
+    ls :: [Text] <- read8 fn textLineType  -- split in lines
 
-            when debug $ putIOwords ["procTxt ls", showT ls]
-            let ls2 = map (procLine2 erl2) ls
-            when debug $ putIOwords ["procTxt ls2", showT ls2]
-            let ls3 = unwrap7 ls2 :: Text
-            when debug $ putIOwords ["procTxt unwrap7 . ls3", showT ls3]
+    -- when debug $ putIOwords ["procTxt ls", showT ls]
+    let ls2 = map (procLine2 erl2) ls
+    -- when debug $ putIOwords ["procTxt ls2", showT ls2]
+    let ls3 = unwrap7 ls2 :: Text
+    -- when debug $ putIOwords ["procTxt unwrap7 . ls3", showT ls3]
 
-            let res = ls2
-            if debug 
-                then write8 fn textlinesNewFile res
-                else do 
+    let res = ls2
+    if debug 
+        then write8 fn textlinesNewFile res
+        else do 
+            renameToBak8 fn textLineType 
+            write8 fn textLineType res 
 
-                    renameToBak8 fn textlinesFile 
-                    write8 fn textlinesFile res 
-        else putIOwords ["ERROR: not txt file - nothing done!"]
-
-    -- rest is copied
-    -- if debug
-    --     then do
-    --     let fnnew = makeAbsFile (toFilePath fn <> "NEW")
-    --     putIOwords ["procMd result in new", showT fnnew]
-    --     write8 fnnew textlinesFile res
-    --     putIOwords ["procMd result in new written", showT res, "fn", showT fnnew]
-    --     else do
-    --     let fnrename = fn <.> bakExtension :: Path Abs File
-    --     renameOneFile (fn <.> txtExtension) fnrename
-    --     putIOwords ["procMd renamed to bak", showT fnrename]
-    -- write8 fn textlinesFile ls2
